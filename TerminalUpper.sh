1d3ca648726a6c1eac649c22710c2b7873de9114
trypen <yutank130e39d@gmail.com>
2024-11-30 19:39:48 +0900

    Update TerminalUpper.sh

--- TerminalUpper.sh
+++ TerminalUpper.sh
@@ -4,7 +4,7 @@
 echo "peco installing..."
 wget -q https://github.com/peco/peco/releases/download/v0.5.11/peco_linux_amd64.tar.gz
 tar zfx peco_linux_amd64.tar.gz peco_linux_amd64/peco
-mv peco_linux_amd64/peco .
+mv peco_linux_amd64/peco /usr/local/bin
 rm peco_linux_amd64.tar.gz
 rm -r peco_linux_amd64
 echo "complete."
